19ab5c49223cf374f561e60d11e6a887a19e37cb
Xuechun Hua <hua_xuechun@outlook.com>
2023-08-20 22:27:01 -0700

    first-commit

---
+++ e-commerce-app/src/test/java/xuechun/springboot/ecommerceapp/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package xuechun.springboot.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
