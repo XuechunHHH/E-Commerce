2b7eecee71a21c8d9d93ff331abfbf50fb62c85a
Xuechun Hua <122423248+XuechunHHH@users.noreply.github.com>
2023-09-13 16:30:06 -0700

    home

--- ecommerce-ui/src/router/index.js
+++ ecommerce-ui/src/router/index.js
@@ -6,6 +6,7 @@ import EditCategory from "../views/Category/EditCategory.vue";
 import AddProduct from "../views/Product/AddProduct.vue";
 import Product from "../views/Product/Product.vue";
 import EditProduct from "../views/Product/EditProduct.vue";
+import ShowDetails from "@/views/Product/ShowDetails.vue";
 
 const routes = [
   {
@@ -42,6 +43,11 @@ const routes = [
     path: "/admin/product/:id",
     name: "EditProduct",
     component: EditProduct,
+  },
+  {
+    path : '/product/show/:id',
+    name : 'ShowDetails',
+    component: ShowDetails
   }
 ];
 
